dedbbb32842b75e10b63a2f2e2fa1d317690b449
Silvestrin <silvestrineschembach@gmail.com>
2023-10-30 18:27:25 -0300

    add readme

--- sql/script.sql
+++ sql/script.sql
@@ -8,20 +8,22 @@ create table usuarios (
     senha varchar(255));
 
 use lillac;    
-create table prestadoresDeServico(
+create table prestadoresdeservico(
     id int primary key auto_increment,
     titulo varchar(255),
     numero varchar(255),
     email varchar(255),
-    descricao varchar(255));
+    descricao varchar(255)),
+    adm int;
 
 use lillac;
-create table tecnologiasAssistivas(
+create table tecnologiasassistivas(
     id int primary key auto_increment,
     titulo varchar(255),
     descricao text(255),
     link varchar(255),
-    empresaResponsavel varchar(255));
+    empresaResponsavel varchar(255)),
+    adm int;
 
 use lillac;
 create table empresas(
@@ -29,4 +31,9 @@ create table empresas(
     titulo varchar(255),
     descricao text(255),
     cnpj varchar(255),
-    endereco varchar(255));
+    endereco varchar(255)),
+    adm int;
+
+use lillac;
+INSERT INTO usuarios(nome, email, senha, adm)
+VALUES ('Nome do adm', 'emaildoadm@gmail.com', '1234', 1);
